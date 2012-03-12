9c296cb437967480f77358850f4c7a3ead870856
Kayle <amber.kayle.armstrong@gmail.com>
2012-03-11 17:13:27 -0700

    Tried to get the updater working and failed. Have commented out the update code in the widget.

--- upcoming-posts-html5.php
+++ upcoming-posts-html5.php
@@ -11,22 +11,24 @@
  
 /* 
  * For the github updater
- */
+
 if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
   include_once('updater.php');
   $config = array(
         'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
         'proper_folder_name' => 'Upcoming-Posts-HTML5', // this is the name of the folder your plugin lives in
         'api_url' => 'https://api.github.com/repos/amberkayle/Upcoming-Posts-HTML5', // the github API url of your github repo
-        'raw_url' => 'https://raw.github.com/amberkayle/Upcoming-Posts-HTML5', // the github raw url of your github repo
+        'raw_url' => 'https://raw.github.com/amberkayle/Upcoming-Posts-HTML5/master', // the github raw url of your github repo
         'github_url' => 'https://github.com/amberkayle/Upcoming-Posts-HTML5', // the github url of your github repo
-        'zip_url' => 'https://github.com/amberkayle/Upcoming-Posts-HTML5/zipball/master', // the zip url of the github repo
+        'zip_url' => 'https://api.github.com/repos/amberkayle/Upcoming-Posts-HTML5/zipball/master', // the zip url of the github repo
         'sslverify' => false, // wether WP should check the validity of the SSL cert when getting an update
         'requires' => '3.0', // which version of WordPress does your plugin require?
         'tested' => '3.3', // which version of WordPress is your plugin tested up to?
     );
     new wp_github_updater($config);
 }
+*/
+
 /* 
  * Protection
  * This string of code will prevent hacks from accessing the file directly.
